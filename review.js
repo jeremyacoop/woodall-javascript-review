11d19e3d3ddfb9f743d9f60c44c8a76becaabb8f
jeremyacoop <63517273+jeremyacoop@users.noreply.github.com>
2021-12-06 11:10:15 -0600

    Wrote special character guide

---
+++ review.js
@@ -0,0 +1,17 @@
+/*
+Jargon!
+. - dot
+()  - parentheses, parens
+[]  - square braces/brackets
+{}  - square braces/brackets
+<   - less than, bird, etc
+>   - greater than, gator
+/   - slash, forward slash, whack
+\   - backslash
+!   - not, bang
+#   - hash, pound, octothorp
+*   - star, asterisk, splat
+||  - or, pipes
+$   - bling, cash money, jQuery
+NaN - not a number, bread
+*/
